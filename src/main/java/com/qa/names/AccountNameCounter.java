735d45158f9ff6ff783c1a0946c5b234c4857789
ScaredTuna <jordanamcr@gmail.com>
2018-03-28 10:00:27 +0100

    Implemented AccountNameCounter method and fixed JUnit test

--- src/main/java/com/qa/names/AccountNameCounter.java
+++ src/main/java/com/qa/names/AccountNameCounter.java
@@ -5,10 +5,17 @@ import java.util.Map;
 import com.qa.account.Account;
 
 public class AccountNameCounter {
+	
+	private int numberAccountsWithName;
 
-	public int countNumberAccountsWithName(String string, Map<Integer, Account> map) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int countNumberAccountsWithName(String name, Map<Integer, Account> accountMap) {
+		numberAccountsWithName = 0;
+		for(int accountNumber:accountMap.keySet()) {
+			if(accountMap.get(accountNumber).getFirstName().equals(name)) {
+				numberAccountsWithName++;
+			}
+		}
+		return numberAccountsWithName;
 	}
 
 }
--- src/test/java/com/qa/names/AccountNameCounterTest.java
+++ src/test/java/com/qa/names/AccountNameCounterTest.java
@@ -21,10 +21,13 @@ public class AccountNameCounterTest {
 		int accountsSameName = counter.countNumberAccountsWithName("Joe", service.getAccountList());
 		assertEquals(0, accountsSameName);
 		Account account1 = new Account("Joe", "Bloggs", 123);
+		service.addAccount(account1);
 		accountsSameName = counter.countNumberAccountsWithName("Joe", service.getAccountList());
 		assertEquals(1, accountsSameName);
 		Account account2 = new Account("Jane", "Bloggs", 234);
 		Account account3 = new Account("Joe", "Smith", 345);
+		service.addAccount(account2);
+		service.addAccount(account3);
 		accountsSameName = counter.countNumberAccountsWithName("Joe", service.getAccountList());
 		assertEquals(2, accountsSameName);
 	}
