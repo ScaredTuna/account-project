b36c392cdd26073ecfca6471d40e064542800770
ScaredTuna <jordanamcr@gmail.com>
2018-03-28 11:32:36 +0100

    Added method to count accounts by last name and test

--- src/main/java/com/qa/names/AccountNameCounter.java
+++ src/main/java/com/qa/names/AccountNameCounter.java
@@ -5,8 +5,12 @@ import com.qa.account.Account;
 
 public class AccountNameCounter {
 
-	public long countNumberAccountsWithName(String name, Map<Integer, Account> accountMap) {
+	public long countNumberAccountsWithFirstName(String name, Map<Integer, Account> accountMap) {
 		return accountMap.values().stream().filter(entry -> entry.getFirstName().equals(name)).count();
 	}
 
+	public long countNumberAccountsWithLastName(String name, Map<Integer, Account> accountMap) {
+		return accountMap.values().stream().filter(entry -> entry.getLastName().equals(name)).count();
+	}
+	
 }
