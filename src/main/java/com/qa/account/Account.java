0b5dc57fafbab2212ae3c4fef3696edafe11e3ec
ScaredTuna <jordanamcr@gmail.com>
2018-04-03 16:32:08 +0100

    Persistence feature using h2 database

--- src/main/java/com/qa/account/Account.java
+++ src/main/java/com/qa/account/Account.java
@@ -1,10 +1,21 @@
 package com.qa.account;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Account {
 	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	@Column(length=45)
 	private String firstName;
+	@Column(length=45)
 	private String lastName;
+	@Column(length=8)
 	private int accountNumber;
 	
 	public Account(String firstName, String lastName, int accountNumber) {
